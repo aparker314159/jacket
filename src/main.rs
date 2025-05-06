04d47fc08f2f4c42077e3f66e8d938a30d0207b0
aparker <aparker314159@gmail.com>
2025-05-06 15:00:23 -0400

    start stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod parser;
+
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let src_file = match std::env::args().nth(1) {
+        Some(s) => s,
+        None => panic!("No source file provided!"),
+    };
+
+    let src = std::fs::read_to_string(src_file).unwrap();
+
+
+
 }
