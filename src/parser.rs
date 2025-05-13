f3971f958a9f24aedad33f1c675b935c1349102d
aparker <aparker314159@gmail.com>
2025-05-12 20:28:42 -0400

    more parsing

--- src/parser.rs
+++ src/parser.rs
@@ -23,12 +23,18 @@ pub enum Primitive {
     /* Prim 2 */
 }
 
+type Iden = String;
+
+
 #[derive(Debug)]
 pub enum Expr {
-    Lit(Value),
-    PrimN(Primitive, Vec<Expr>),
-    App(Box<Expr>, Vec<Expr>),
+    Lit{v: Value},
+    Identifier{id: Iden},
+    PrimN{prim: Primitive, args: Vec<Expr>},
+    If{if_: Box<Expr>, then_: Box<Expr>, else_: Box<Expr>},
+    App{func: Box<Expr>, args: Vec<Expr>},
 }
+
 
 
 fn parse_bool<'src>() -> impl Parser<'src, &'src str, Value, extra::Err<Simple<'src, char>>> + Clone {
@@ -59,17 +65,34 @@ fn parse_value<'src>() -> impl Parser<'src, &'src str, Value, extra::Err<Simple<
 
 fn parse_prim_name<'src>() -> impl Parser<'src, &'src str, Primitive, extra::Err<Simple<'src, char>>> + Clone {
     choice((
-        text::ascii::keyword("void").to(Primitive::Void),
-        text::ascii::keyword("read-byte").to(Primitive::ReadByte),
-        text::ascii::keyword("peek-byte").to(Primitive::PeekByte),
-        text::ascii::keyword("add1").to(Primitive::Add1),
-        text::ascii::keyword("sub1").to(Primitive::Sub1),
-        text::ascii::keyword("zero?").to(Primitive::IsZero),
-        text::ascii::keyword("char?").to(Primitive::IsChar),
-        text::ascii::keyword("integer->char").to(Primitive::IntToChar),
-        text::ascii::keyword("char->int").to(Primitive::CharToInt),
-        text::ascii::keyword("write-byte").to(Primitive::WriteByte),
+        just("void").to(Primitive::Void),
+        just("read-byte").to(Primitive::ReadByte),
+        just("peek-byte").to(Primitive::PeekByte),
+        just("add1").to(Primitive::Add1),
+        just("sub1").to(Primitive::Sub1),
+        just("zero?").to(Primitive::IsZero),
+        just("char?").to(Primitive::IsChar),
+        just("integer->char").to(Primitive::IntToChar),
+        just("char->int").to(Primitive::CharToInt),
+        just("write-byte").to(Primitive::WriteByte),
     ))
+}
+
+fn is_identifier_char(c: &char) -> bool {
+    !("()[]{}\",'`;#|\\ \t\n\r\x0B\x0C".contains(*c))
+}
+
+fn parse_iden_expr<'src>() -> impl Parser<'src, &'src str, Expr, extra::Err<Simple<'src, char>>> + Clone {
+    (any().filter(is_identifier_char)).repeated().at_least(1).collect::<String>()
+        .map(|s| {Expr::Identifier{id: s.to_string()}})
+}
+
+fn parse_open<'src>() -> impl Parser<'src, &'src str, char, extra::Err<Simple<'src, char>>> + Clone {
+    one_of("([{") 
+}
+
+fn parse_close<'src>() -> impl Parser<'src, &'src str, char, extra::Err<Simple<'src, char>>> + Clone {
+    one_of(")]}") 
 }
 
 pub fn parse_expr<'src>() -> impl Parser<'src, &'src str, Expr, extra::Err<Simple<'src, char>>> + Clone {
@@ -77,15 +100,27 @@ pub fn parse_expr<'src>() -> impl Parser<'src, &'src str, Expr, extra::Err<Simpl
         let parse_prim = (parse_prim_name()
             .then_ignore(text::whitespace())
             .then(expr.clone().separated_by(text::whitespace()).collect::<Vec<_>>()))
-            .delimited_by(just('('), just(')'))
-            .map(|(prim, args)| { Expr::PrimN(prim, args) });
+            .delimited_by(parse_open(), parse_close())
+            .map(|(prim, args)| { Expr::PrimN{prim, args} });
 
         let parse_app = (expr.clone()
             .then_ignore(text::whitespace())
-            .then(expr.separated_by(text::whitespace()).collect::<Vec<_>>()))
-            .delimited_by(just('('), just(')'))
-            .map(|(e, args)| { Expr::App(Box::new(e), args) });
+            .then(expr.clone().separated_by(text::whitespace()).collect::<Vec<_>>()))
+            .delimited_by(parse_open(), parse_close())
+            .map(|(e, args)| { Expr::App{func: Box::new(e), args} });
 
-        (parse_value().map(|v| {Expr::Lit(v)})).or(parse_prim).or(parse_app)
+        let parse_if = (
+            just("if").then_ignore(text::whitespace())
+            .then(expr.clone()).then_ignore(text::whitespace())
+            .then(expr.clone()).then_ignore(text::whitespace())
+            .then(expr.clone()).then_ignore(text::whitespace())
+            ).delimited_by(parse_open(), parse_close())
+            .map(|(((_, if_), then_), else_)| {Expr::If { if_: Box::new(if_), then_: Box::new(then_), else_: Box::new(else_)}} );
+
+        (parse_value().map(|v| { Expr::Lit{v} }))
+            .or(parse_iden_expr())
+            .or(parse_if)
+            .or(parse_prim)
+            .or(parse_app)
     })
 }
